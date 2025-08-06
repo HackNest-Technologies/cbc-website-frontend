84e518f6fa09ba3a5eaa817e5d7aa427f4e6ef18
JimOluwaseyi <heysey128@gmail.com>
2025-08-06 22:20:05 +0100

    Initiating environment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      {/* Define your routes here */}
+      <Route  index element={<HomePage/>} />
+    </Route>
+  )
+);
 
 function App() {
-
-  return (
-    <>
-      <div className="container mx-auto">
-        </div>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;

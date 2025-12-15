9b20a6b227bc9a2e36d1a3e939e97a1fca71dcb4
JimOluwaseyi <heysey128@gmail.com>
2025-12-15 12:05:53 +0100

    Resolve import issue

--- src/pages/LiveChatPage.jsx
+++ src/pages/LiveChatPage.jsx
@@ -1,11 +1,11 @@
 import LiveChat from "../components/LiveChat/LiveChat";
-import LoginPage from "../components/LiveChat/LoginPage";
+// import LoginPage from "../components/LiveChat/LoginPage";
 
 const LiveChatPage = () => {
   return (
     <section>
       <LiveChat />
-      <LoginPage />
+      {/* <LoginPage /> */}
     </section>
   );
 };
